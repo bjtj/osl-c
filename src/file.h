7211b07bbecd048055db8ae70d23df1f6dd5c8c6
bjtj <bjtj10@gmail.com>
2018-10-18 18:16:33 +0900

    added: file

---
+++ src/file.h
@@ -0,0 +1,19 @@
+#ifndef __FILE_H__
+#define __FILE_H__
+
+#include "osl.h"
+
+#if defined(USE_UNIX_STD)
+typedef off_t osl_filesize_t;
+#elif defined(USE_MS_WIN)
+typedef unsigned long long osl_filesize_t;
+#else
+// unknown platform
+typedef unsigned long osl_filesize_t;
+#endif
+
+osl_filesize_t osl_file_size(const char * path);
+char * osl_file_dump(FILE * fp);
+
+
+#endif
