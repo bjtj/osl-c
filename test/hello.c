6e30fc018eb3dcc0623430537bea38edfe9a08e0
bjtj <bjtj10@gmail.com>
2018-10-22 03:51:41 +0900

    done: windows test

--- test/hello.c
+++ test/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hello(void)
+#if defined(_WIN32) || defined(_WIN64) /* windows */
+#	define OSL_EXPORT __declspec(dllexport)
+#endif
+
+void OSL_EXPORT hello(void)
 {
     printf("hello!!!\n");
 }
