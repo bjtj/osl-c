e35e55b6b2a0e3d8ea204c23a800acaeba296819
bjtj <bjtj10@gmail.com>
2021-01-10 18:53:12 +0900

    updated: network - msvc

--- test/test_network.c
+++ test/test_network.c
@@ -1,5 +1,5 @@
 #include "network.h"
-#include "osl.h"
+#include "inet_address.h"
 #include <assert.h>
 
 void test_network(void)
@@ -9,8 +9,16 @@ void test_network(void)
     osl_list_t * ptr = ifaces;
     for (; ptr; ptr = ptr->next)
     {
+        osl_list_t* addr_ptr;
+
 	osl_network_interface_t * iface = (osl_network_interface_t*)ptr->data;
-	printf("* iface name -- %s\n", iface->name);
+	printf("* Network interface name: %s (loopback: %s)\n", iface->name, iface->is_loopback ? "Y" : "N");
+    addr_ptr = iface->addr_list;
+    for (; addr_ptr; addr_ptr = addr_ptr->next) {
+        osl_inet_address_t* addr = (osl_inet_address_t*)addr_ptr->data;
+        printf(" - IP: %s\n", addr->host);
+    }
+
     }
 
     osl_list_free(ifaces, (void (*)(void*))osl_network_interface_free);
