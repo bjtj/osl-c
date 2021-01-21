1ea4d93fd4c93a670bf6bb9a24c24ca9dc24f9ad
bjtj <bjtj10@gmail.com>
2021-01-22 08:00:59 +0900

    updated: selector

--- src/selector.c
+++ src/selector.c
@@ -1,5 +1,24 @@
 #include "selector.h"
 
+osl_selector_t * osl_selector_new(void)
+{
+    osl_selector_t * selector = (osl_selector_t *)malloc(sizeof(osl_selector_t));
+    OSL_HANDLE_MALLOC_ERROR(selector);
+    memset(selector, 0, sizeof(osl_selector_t));
+    return selector;
+}
+
+osl_selector_t * osl_selector_init(osl_selector_t * selector)
+{
+    osl_selector_clear(selector);
+    return selector;
+}
+
+void osl_selector_free(osl_selector_t * selector)
+{
+    osl_safe_free(selector);
+}
+
 void osl_selector_clear(osl_selector_t * selector)
 {
     memset(selector, 0, sizeof(osl_selector_t));
@@ -15,15 +34,15 @@ void osl_selector_register(osl_selector_t * selector, osl_socket fd, int flags)
 	selector->maxfds = fd;
     }
 
-    if (flags & osl_read)
+    if (flags & OSL_FLAG_READ)
     {
 	FD_SET(fd, &(selector->readfds));
     }
-    if (flags & osl_write)
+    if (flags & OSL_FLAG_WRITE)
     {
 	FD_SET(fd, &(selector->writefds));
     }
-    if (flags & osl_except)
+    if (flags & OSL_FLAG_EXCEPT)
     {
 	FD_SET(fd, &(selector->exceptfds));
     }
@@ -31,15 +50,15 @@ void osl_selector_register(osl_selector_t * selector, osl_socket fd, int flags)
 
 void osl_selector_unregister(osl_selector_t * selector, osl_socket fd, int flags)
 {
-    if (flags & osl_read)
+    if (flags & OSL_FLAG_READ)
     {
 	FD_CLR(fd, &(selector->readfds));
     }
-    if (flags & osl_write)
+    if (flags & OSL_FLAG_WRITE)
     {
 	FD_CLR(fd, &(selector->writefds));
     }
-    if (flags & osl_except)
+    if (flags & OSL_FLAG_EXCEPT)
     {
 	FD_CLR(fd, &(selector->exceptfds));
     }
