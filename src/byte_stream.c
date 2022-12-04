7b4c8c026e078fbe434c243da1c998fa64e35c48
bjtj <bjtj10@gmail.com>
2022-12-04 20:36:02 +0900

    udpated: byte stream read buffer

--- src/byte_stream.c
+++ src/byte_stream.c
@@ -18,13 +18,26 @@ static int s_read_cb(osl_stream_t * stream)
   return *(con->ptr++);
 }
 
+static size_t s_read_buffer_cb(osl_stream_t * stream, void * buf, size_t size)
+{
+  size_t i;
+  for (i = 0; i < size; ++i) {
+    int ret = s_read_cb(stream);
+    if (ret == -1) {
+      break;
+    }
+    ((uint8_t*)buf)[i] = (uint8_t)ret;
+  }
+  return i;
+}
+
 static int s_write_cb(osl_stream_t * stream, int ch)
 {
   (void)stream;
   (void)ch;
   /* todo: */
     
-  return 0;
+  return -1;
 }
 
 static void s_close_cb(osl_stream_t * stream)
@@ -55,6 +68,7 @@ osl_stream_t * osl_byte_stream_init(osl_stream_t * stream, uint8_t * data, size_
   stream->handle = con;
   stream->eof = 0;
   stream->read = s_read_cb;
+  stream->read_buffer = s_read_buffer_cb;
   stream->write = s_write_cb;
   stream->close = s_close_cb;
   return stream;
--- src/string_stream.c
+++ src/string_stream.c
@@ -3,57 +3,61 @@
 
 struct container
 {
-    char * str;
-    char * ptr;
+  char * str;
+  char * ptr;
 };
 
 static int s_read_cb(osl_stream_t * stream)
 {
-    struct container * con = (struct container*)stream->handle;
-    char ch = *(con->ptr++);
-    if (ch == 0)
-    {
-	stream->eof = 1;
-    }
-    return ch;
+  struct container * con = (struct container*)stream->handle;
+  if (stream->eof) {
+    return -1;
+  }
+  char ch = *(con->ptr++);
+  if (ch == 0)
+  {
+    stream->eof = 1;
+    return -1;
+  }
+  return ch;
 }
 
 static int s_write_cb(osl_stream_t * stream, int ch)
 {
-    (void)stream;
-    (void)ch;
-    /* todo: */
+  (void)stream;
+  (void)ch;
+  /* todo: */
     
-    return 0;
+  return -1;
 }
 
 static void s_close_cb(osl_stream_t * stream)
 {
-    struct container * con = (struct container*)stream->handle;
-    if (con)
-    {
-	osl_safe_free(con->str);
-	osl_safe_free(con);
-	stream->handle = NULL;
-    }
+  struct container * con = (struct container*)stream->handle;
+  if (con)
+  {
+    osl_safe_free(con->str);
+    osl_safe_free(con);
+    stream->handle = NULL;
+  }
 }
 
 
 osl_stream_t * osl_string_stream_init(osl_stream_t * stream, const char * str)
 {
-    struct container * con;
-    if (str == NULL) {
-	return NULL;
-    }
-    con = (struct container*)malloc(sizeof(struct container));
-    OSL_HANDLE_MALLOC_ERROR(con);
-    memset(con, 0, sizeof(struct container));
-    con->str = osl_safe_strdup(str);
-    con->ptr = con->str;
-    stream->handle = con;
-    stream->eof = 0;
-    stream->read = s_read_cb;
-    stream->write = s_write_cb;
-    stream->close = s_close_cb;
-    return stream;
+  struct container * con;
+  if (str == NULL) {
+    return NULL;
+  }
+  con = (struct container*)malloc(sizeof(struct container));
+  OSL_HANDLE_MALLOC_ERROR(con);
+  memset(con, 0, sizeof(struct container));
+  con->str = osl_safe_strdup(str);
+  con->ptr = con->str;
+  stream->handle = con;
+  stream->eof = 0;
+  stream->read = s_read_cb;
+  stream->write = s_write_cb;
+  stream->close = s_close_cb;
+  return stream;
 }
